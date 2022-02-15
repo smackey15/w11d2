c5b0b0bf6b84e0422a9de7d1bd24f839eb43d9bc
sean <midruss@yahoo.com>
2022-02-15 15:27:32 -0500

    Imported store to entry file and tested getState on the window

--- todo/frontend/reducers/todos_reducer.js
+++ todo/frontend/reducers/todos_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_TODOS, RECEIVE_TODO } from "../actions/todo_actions";
 
-const todosReducer = (state = {}, action) => {
+const todosReducer = (state = initialState, action) => {
     Object.freeze(state);
     
     switch (action.type) {
@@ -8,5 +8,20 @@ const todosReducer = (state = {}, action) => {
         return state;
     }
 };
-  
+
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
 export default todosReducer;
--- todo/todo_redux.jsx
+++ todo/todo_redux.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from "./frontend/store/store";
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(<h1>Todos App</h1>, document.getElementById('root'));
+    const store = configureStore();
+    window.store = store;
 });
