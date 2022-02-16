93cd99ecd2198015ad96b3d4bcb19139193318ff
yuhmanashi <yzxu03@gmail.com>
2022-02-16 09:04:53 -0800

    finished pt2 phase1

--- todo/todo_redux.jsx
+++ todo/todo_redux.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from "./frontend/store/store";
 import { receiveTodo, receiveTodos, removeTodo } from "./frontend/actions/todo_actions";
+import Root from "./frontend/components/root"
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<h1>Todos App</h1>, document.getElementById('root'));
     const store = configureStore();
+    ReactDOM.render(<Root store={ store }/>, document.getElementById('root'));
     window.store = store;
     window.receiveTodos = receiveTodos;
     window.receiveTodo = receiveTodo;
