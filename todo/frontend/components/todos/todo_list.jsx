0267ff570347abf955532899867ab910454b8e56
sean <midruss@yahoo.com>
2022-02-16 15:28:04 -0500

    Finished todo list and todo list item

--- todo/frontend/components/todos/todo_list.jsx
+++ todo/frontend/components/todos/todo_list.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { TodoListItem } from '../todo_list/todo_list_item';
 
-export default () => (<h3>Todo List goes here!</h3>)
+export default (props) => (
+    <div>
+        <h3>Todo List goes here!</h3>
+        {TodoListItem(props)};
+    </div>
+)
+
