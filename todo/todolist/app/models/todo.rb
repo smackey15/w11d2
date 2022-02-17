bc56864851a1d0d455f9782ded6c3c0d3f191f5b
sean <midruss@yahoo.com>
2022-02-17 14:38:15 -0500

    tested db setup by creating new todos

--- todo/todolist/app/models/todo.rb
+++ todo/todolist/app/models/todo.rb
@@ -1,2 +1,5 @@
 class Todo < ApplicationRecord
+    validates :body, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :done, inclusion: { in: [true, false] }
 end
