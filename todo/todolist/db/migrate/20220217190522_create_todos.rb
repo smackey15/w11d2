2cad806baf01e466b336782e8584d010bc712b56
yuhmanashi <yzxu03@gmail.com>
2022-02-17 11:14:25 -0800

    finished setup

---
+++ todo/todolist/db/migrate/20220217190522_create_todos.rb
@@ -0,0 +1,13 @@
+class CreateTodos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :todos do |t|
+      t.string :title, null: false
+      t.string :body, null: false
+      t.boolean :done, null: false, default: false
+
+      t.timestamps
+    end
+
+    add_index :todos, :title, unique: true
+  end
+end
